26984e16c583955345bd9e3e68b0494322e77106
“Ruslan <callogan217@gmail.com>
2023-12-02 23:00:27 +0200

    Make amendments to views, local urls and templates to implement the logic of deleting tasks

--- task/urls.py
+++ task/urls.py
@@ -1,14 +1,28 @@
 from django.urls import path
 
-from task.views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView
+from task.views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView, toggle_task_completion
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path(
-        "task/create-task/", TaskCreateView.as_view(), name='create-task'
+        "task/create-task/",
+        TaskCreateView.as_view(),
+        name='create-task'
     ),
     path(
-        "task/<int:pk>/update/", TaskUpdateView.as_view(), name='update-task'
+        "task/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name='update-task'
+    ),
+    path(
+        "task/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="delete-task",
+    ),
+    path(
+        "task/<int:pk>/toggle_completion/",
+        toggle_task_completion,
+        name="toggle-task-completion"
     ),
 ]
 
