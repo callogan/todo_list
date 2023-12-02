eaa97cf0f41a12d85c85aa47ed13a4b5eefd122c
“Ruslan <callogan217@gmail.com>
2023-12-02 19:10:42 +0200

    Enhance task create and update views with subsequent addition of local urls

--- task/urls.py
+++ task/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from task.views import TaskListView, TaskCreateView
+from task.views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path(
-        "create-task/", TaskCreateView.as_view(), name='create-task'
+        "task/create-task/", TaskCreateView.as_view(), name='create-task'
+    ),
+    path(
+        "task/<int:pk>/update/", TaskUpdateView.as_view(), name='update-task'
     ),
 ]
 
--- task/views.py
+++ task/views.py
@@ -8,18 +8,23 @@ class TaskListView(generic.ListView):
     model = Task
     context_object_name = "task_list"
     template_name = "task/task_list.html"
+    queryset = Task.objects.select_related("tags").prefetch_related("tags")
 
 
 class TaskCreateView(generic.CreateView):
     model = Task
     fields = "__all__"
-    template_name = "task/task_list.html"
+    template_name = "task/task_create.html"
     success_url = reverse_lazy("task:task-list")
 
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
+    fields = "__all__"
+    template_name = "task/task_update.html"
+    success_url = reverse_lazy("task:task-list")
 
 
 class TaskDeleteView(generic.DeleteView):
     model = Task
+    success_url = reverse_lazy("task:task-list")
