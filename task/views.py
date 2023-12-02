6ee363aa3f916e243fd730cc672000c25a7987c2
“Ruslan <callogan217@gmail.com>
2023-12-02 18:06:33 +0200

    Make amendments to views

--- task/views.py
+++ task/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -7,11 +6,14 @@ from task.models import Task
 
 class TaskListView(generic.ListView):
     model = Task
+    context_object_name = "task_list"
+    template_name = "task/task_list.html"
 
 
 class TaskCreateView(generic.CreateView):
     model = Task
     fields = "__all__"
+    template_name = "task/task_list.html"
     success_url = reverse_lazy("task:task-list")
 
 
