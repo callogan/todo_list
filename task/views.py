8f6f9fa10f16a3069a476577843820bfb2454159
“Ruslan <callogan217@gmail.com>
2023-12-02 16:20:19 +0200

    Create views

--- task/views.py
+++ task/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from task.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
