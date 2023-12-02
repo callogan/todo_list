346f8b8c09e259b40072c21c8dcbf02eddf1c8f8
“Ruslan <callogan217@gmail.com>
2023-12-02 15:44:26 +0200

    Create models and running migrations

--- task/models.py
+++ task/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField(max_length=100)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True)
+    completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        ordering = ["completed"]
